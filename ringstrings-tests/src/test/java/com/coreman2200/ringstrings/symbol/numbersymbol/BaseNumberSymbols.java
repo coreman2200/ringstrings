c252fbde95f26232942c86d6dde485fb8b9de08f
Cory Higginbottom <coreman2200@gmail.com>
2015-05-25 00:11:05 -0700

    removal of generated files from git repo

---
+++ ringstrings-tests/src/test/java/com/coreman2200/ringstrings/symbol/numbersymbol/BaseNumberSymbols.java
@@ -0,0 +1,35 @@
+package com.coreman2200.ringstrings.symbol.numbersymbol;
+
+import com.coreman2200.ringstrings.symbol.ISymbol;
+
+/**
+ * BaseNumberSymbol
+ * Enum details available number symbols within Numerology contexts.
+ *
+ * Created by Cory Higginbottom on 5/24/15
+ * http://github.com/coreman2200
+ *
+ * Licensed under the GNU General Public License (GPL), Version 2.0.
+ * You may not use this file except in compliance with this License.
+ *
+ * You may obtain a copy of the GPLv2 License at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+public enum BaseNumberSymbols {
+    ZERO(0),
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    ELEVEN,
+    TWENTY_TWO,
+    THIRTY_THREE,
+    FOURTY_FOUR,
+
+}
