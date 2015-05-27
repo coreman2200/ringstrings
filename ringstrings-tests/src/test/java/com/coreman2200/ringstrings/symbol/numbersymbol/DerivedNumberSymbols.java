a0573cd9491ee888c21de852d51830da21471b28
Cory Higginbottom <coreman2200@gmail.com>
2015-05-26 20:59:04 -0700

    Defines some derived number symbols (Karmic Debt ~ will see whether to split apart if more arise)

--- ringstrings-tests/src/test/java/com/coreman2200/ringstrings/symbol/numbersymbol/DerivedNumberSymbols.java
+++ ringstrings-tests/src/test/java/com/coreman2200/ringstrings/symbol/numbersymbol/DerivedNumberSymbols.java
@@ -2,17 +2,36 @@ package com.coreman2200.ringstrings.symbol.numbersymbol;
 
 /**
  * DerivedNumberSymbols
- * description
- * <p/>
+ * Enum for some derived number symbols of note.
+ *
  * Created by Cory Higginbottom on 5/26/15
  * http://github.com/coreman2200
- * <p/>
+ *
  * Licensed under the GNU General Public License (GPL), Version 2.0.
  * You may not use this file except in compliance with this License.
- * <p/>
+ *
  * You may obtain a copy of the GPLv2 License at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
-public class DerivedNumberSymbols {
+public enum DerivedNumberSymbols {
+    KARMICDEBT10(10),
+    KARMICDEBT13(13),
+    KARMICDEBT14(14),
+    KARMICDEBT16(16),
+    KARMICDEBT19(19);
+
+    private final int derivedValue;
+
+    DerivedNumberSymbols(int value) {
+        derivedValue = value;
+    }
+
+    static public boolean isValueDerivedNumberSymbol(int value) {
+        for (DerivedNumberSymbols symbol : DerivedNumberSymbols.values()) {
+            if (symbol.derivedValue == value)
+                return true;
+        }
+        return false;
+    }
 }
