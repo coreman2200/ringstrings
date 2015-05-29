9ce50a9844a3f3f0e4ec8dfd10d2cdca0de9a502
Cory Higginbottom <coreman2200@gmail.com>
2015-05-28 22:23:08 -0700

    Enums for specified grouped number symbols

--- ringstrings-tests/src/test/java/com/coreman2200/ringstrings/symbol/numbersymbol/grouped/Challenges.java
+++ ringstrings-tests/src/test/java/com/coreman2200/ringstrings/symbol/numbersymbol/grouped/Challenges.java
@@ -2,17 +2,21 @@ package com.coreman2200.ringstrings.symbol.numbersymbol.grouped;
 
 /**
  * Challenges
- * description
- * <p/>
+ * Enum list of challenges..
+ *
  * Created by Cory Higginbottom on 5/27/15
  * http://github.com/coreman2200
- * <p/>
+ *
  * Licensed under the GNU General Public License (GPL), Version 2.0.
  * You may not use this file except in compliance with this License.
- * <p/>
+ *
  * You may obtain a copy of the GPLv2 License at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
 public enum Challenges {
+    CHALLENGE1,
+    CHALLENGE2,
+    CHALLENGE3,
+    CHALLENGE4
 }
--- ringstrings-tests/src/test/java/com/coreman2200/ringstrings/symbol/numbersymbol/grouped/Periods.java
+++ ringstrings-tests/src/test/java/com/coreman2200/ringstrings/symbol/numbersymbol/grouped/Periods.java
@@ -3,16 +3,19 @@ package com.coreman2200.ringstrings.symbol.numbersymbol.grouped;
 /**
  * Periods
  * description
- * <p/>
+ *
  * Created by Cory Higginbottom on 5/27/15
  * http://github.com/coreman2200
- * <p/>
+ *
  * Licensed under the GNU General Public License (GPL), Version 2.0.
  * You may not use this file except in compliance with this License.
- * <p/>
+ *
  * You may obtain a copy of the GPLv2 License at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
 public enum Periods {
+    CYCLE1,
+    CYCLE2,
+    CYCLE3
 }
