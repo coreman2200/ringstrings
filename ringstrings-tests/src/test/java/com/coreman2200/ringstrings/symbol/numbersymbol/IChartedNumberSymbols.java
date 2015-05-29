72764919458009494590dec6caba671382ab7942
Cory Higginbottom <coreman2200@gmail.com>
2015-05-28 22:28:12 -0700

    IChartedNumberSymbols and corresponding (full) NumerologicalChartImpl

--- ringstrings-tests/src/test/java/com/coreman2200/ringstrings/symbol/numbersymbol/IChartedNumberSymbols.java
+++ ringstrings-tests/src/test/java/com/coreman2200/ringstrings/symbol/numbersymbol/IChartedNumberSymbols.java
@@ -2,17 +2,17 @@ package com.coreman2200.ringstrings.symbol.numbersymbol;
 
 /**
  * IChartedNumberSymbols
- * description
- * <p/>
+ * Interface describing a grouping of number symbols expressed as a Numerological chart.
+ *
  * Created by Cory Higginbottom on 5/28/15
  * http://github.com/coreman2200
- * <p/>
+ *
  * Licensed under the GNU General Public License (GPL), Version 2.0.
  * You may not use this file except in compliance with this License.
- * <p/>
+ *
  * You may obtain a copy of the GPLv2 License at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
-public interface IChartedNumberSymbols {
+public interface IChartedNumberSymbols extends IGroupedNumberSymbols {
 }
--- ringstrings-tests/src/test/java/com/coreman2200/ringstrings/symbol/numbersymbol/NumerologicalChartImpl.java
+++ ringstrings-tests/src/test/java/com/coreman2200/ringstrings/symbol/numbersymbol/NumerologicalChartImpl.java
@@ -2,17 +2,22 @@ package com.coreman2200.ringstrings.symbol.numbersymbol;
 
 /**
  * NumerologicalChartImpl
- * description
- * <p/>
+ * Describes entire numerological chart as groups of numerological symbols.
+ *
  * Created by Cory Higginbottom on 5/28/15
  * http://github.com/coreman2200
- * <p/>
+ *
  * Licensed under the GNU General Public License (GPL), Version 2.0.
  * You may not use this file except in compliance with this License.
- * <p/>
+ *
  * You may obtain a copy of the GPLv2 License at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
-public class NumerologicalChartImpl {
+public class NumerologicalChartImpl extends GroupedNumberSymbolsImpl implements IChartedNumberSymbols {
+    public NumerologicalChartImpl() {
+        super(GroupedNumberSymbols.CHART);
+    }
+
+
 }
