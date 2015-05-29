5a52c1b137b6045d9d22c854c01af760219f0671
Cory Higginbottom <coreman2200@gmail.com>
2015-05-28 22:28:40 -0700

    Weak descript Profile interface

--- ringstrings-tests/src/test/java/com/coreman2200/ringstrings/symbol/IProfile.java
+++ ringstrings-tests/src/test/java/com/coreman2200/ringstrings/symbol/IProfile.java
@@ -1,18 +1,28 @@
 package com.coreman2200.ringstrings.symbol;
 
+import com.coreman2200.ringstrings.numerology.numbersystem.NumberSystemType;
+
 /**
  * IProfile
- * description
- * <p/>
+ * Interface for Profiles (data structures used to store raw user input/data)
+ *
  * Created by Cory Higginbottom on 5/27/15
  * http://github.com/coreman2200
- * <p/>
+ *
  * Licensed under the GNU General Public License (GPL), Version 2.0.
  * You may not use this file except in compliance with this License.
- * <p/>
+ *
  * You may obtain a copy of the GPLv2 License at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
 public interface IProfile {
+    String getFirstName();
+    String getMiddleName();
+    String getLastName();
+    int getBirthDay();
+    int getBirthMonth();
+    int getBirthYear();
+    void genProfile();
+    NumberSystemType getNumberSystem();
 }
