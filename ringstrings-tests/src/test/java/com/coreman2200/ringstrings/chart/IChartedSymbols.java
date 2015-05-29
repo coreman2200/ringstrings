1b6e0341d32b5754ac0de2aa63a6d5243ef959f4
Cory Higginbottom <coreman2200@gmail.com>
2015-05-28 22:16:54 -0700

    ChartedSymbols interface wip

--- ringstrings-tests/src/test/java/com/coreman2200/ringstrings/chart/IChartedSymbols.java
+++ ringstrings-tests/src/test/java/com/coreman2200/ringstrings/chart/IChartedSymbols.java
@@ -2,14 +2,14 @@ package com.coreman2200.ringstrings.chart;
 
 /**
  * IChartedSymbols
- * description
- * <p/>
+ * Interface describing charted symbols in general
+ *
  * Created by Cory Higginbottom on 5/28/15
  * http://github.com/coreman2200
- * <p/>
+ *
  * Licensed under the GNU General Public License (GPL), Version 2.0.
  * You may not use this file except in compliance with this License.
- * <p/>
+ *
  * You may obtain a copy of the GPLv2 License at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
