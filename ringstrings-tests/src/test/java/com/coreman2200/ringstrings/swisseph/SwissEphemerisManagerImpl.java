4235018af1e4a56e03e495d4f739d330a5d228a3
Cory Higginbottom <coreman2200@gmail.com>
2015-06-06 00:22:19 -0700

    removes library handler; replaced with SwissEphemerisManager

---
+++ ringstrings-tests/src/test/java/com/coreman2200/ringstrings/swisseph/SwissEphemerisManagerImpl.java
@@ -0,0 +1,69 @@
+package com.coreman2200.ringstrings.swisseph;
+
+import android.annotation.TargetApi;
+import android.location.Location;
+import android.text.format.Time;
+
+import com.sun.istack.internal.NotNull;
+
+import java.util.GregorianCalendar;
+
+import swisseph.SweDate;
+import swisseph.SwissEph;
+
+/**
+ * SwissEphemerisManagerImpl
+ * Implementation of SwissEphemerisManager.
+ *
+ * Created by Cory Higginbottom on 6/5/15
+ * http://github.com/coreman2200
+ *
+ * Licensed under the GNU General Public License (GPL), Version 2.0.
+ * You may not use this file except in compliance with this License.
+ *
+ * You may obtain a copy of the GPLv2 License at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+public class SwissEphemerisManagerImpl implements ISwissEphemerisManager {
+    private final SwissEph mEphemeris;
+    private SweDate mSwissephDate;
+    private Location mGeoLocation;
+    private double mMaxOrb = 3.0; // Cleanup..
+
+    public SwissEphemerisManagerImpl(String ephepath) {
+        mEphemeris = new SwissEph(ephepath);
+    }
+
+    public void setDate(@NotNull GregorianCalendar date) {
+        if (mSwissephDate == null) {
+            mSwissephDate = new SweDate();
+            mSwissephDate.setCalendarType(SweDate.SE_GREG_CAL, false);
+        }
+
+        mSwissephDate.setJulDay(getJulianDayForDate(date));
+    }
+
+    public void setLocation(@NotNull Location location) {
+            mGeoLocation = location;
+    }
+
+
+
+    @TargetApi(21)
+    private long getJulianDayForDate(GregorianCalendar date) {
+        return Time.getJulianDay(date.getTimeInMillis(), date.getTimeZone().getRawOffset());
+    }
+
+    public void setMaxOrb(double orb)
+    {
+        if (orb >= 0.0D)
+            mMaxOrb = orb;
+    }
+
+    private void closeSwisseph()
+    {
+        mEphemeris.swe_close();
+
+    }
+}
