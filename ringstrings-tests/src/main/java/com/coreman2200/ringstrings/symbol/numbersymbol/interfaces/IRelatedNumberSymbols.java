22b5b915f4d16fe8792519ff0b82ae8fbd86b6df
Cory Higginbottom <coreman2200@gmail.com>
2015-06-05 21:46:38 -0700

    Number Symbol interfaces

---
+++ ringstrings-tests/src/main/java/com/coreman2200/ringstrings/symbol/numbersymbol/interfaces/IRelatedNumberSymbols.java
@@ -0,0 +1,18 @@
+package com.coreman2200.ringstrings.symbol.numbersymbol.interfaces;
+
+/**
+ * IRelatedNumberSymbols
+ * Related number symbols are lists of symbols related per comparison of some shared metric (size, value..)
+ *
+ * Created by Cory Higginbottom on 5/26/15
+ * http://github.com/coreman2200
+ *
+ * Licensed under the GNU General Public License (GPL), Version 2.0.
+ * You may not use this file except in compliance with this License.
+ *
+ * You may obtain a copy of the GPLv2 License at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+public interface IRelatedNumberSymbols {
+}
