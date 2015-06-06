d2721a400b296e5d73ca15cade91cce8e9f5903b
Cory Higginbottom <coreman2200@gmail.com>
2015-06-05 21:50:10 -0700

    Symbol comparator wip

---
+++ ringstrings-tests/src/main/java/com/coreman2200/ringstrings/symbol/symbolcomparator/NumberSymbolValueComparatorImpl.java
@@ -0,0 +1,27 @@
+package com.coreman2200.ringstrings.symbol.symbolcomparator;
+
+import java.util.Comparator;
+import com.coreman2200.ringstrings.symbol.numbersymbol.interfaces.INumberSymbol;
+
+/**
+ * NumberSymbolValueComparatorImpl
+ * Compares number symbol values
+ *
+ * Created by Cory Higginbottom on 5/26/15
+ * http://github.com/coreman2200
+ *
+ * Licensed under the GNU General Public License (GPL), Version 2.0.
+ * You may not use this file except in compliance with this License.
+ *
+ * You may obtain a copy of the GPLv2 License atx
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+ public class NumberSymbolValueComparatorImpl<T> implements Comparator<INumberSymbol> {
+
+    @Override
+    public int compare(INumberSymbol o1, INumberSymbol o2) {
+        return o2.getNumberSymbolValue() - o1.getNumberSymbolValue();
+    }
+
+}
