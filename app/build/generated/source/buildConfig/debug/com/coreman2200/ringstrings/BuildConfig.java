6497846f00e5fd09fae08d71f7d4e00a90ba6c68
Cory H <coreman2200@gmail.com>
2022-01-13 09:17:34 -0800

    build adjustments; separation into app, data, domain, presentation layers/modules

--- app/build/generated/source/buildConfig/debug/com/coreman2200/ringstrings/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/coreman2200/ringstrings/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.coreman2200.ringstrings.test";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "0.1";
+  public static final String VERSION_NAME = "1.0";
 }
