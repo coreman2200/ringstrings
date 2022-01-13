47c850d27838d670fda40e1e82e664e8f98d252f
Cory H <coreman2200@gmail.com>
2022-01-13 15:35:08 -0800

    inits app component and util module (provides context)

--- app/src/main/java/com/coreman2200/ringstrings/app/RSApp.kt
+++ app/src/main/java/com/coreman2200/ringstrings/app/RSApp.kt
@@ -1,6 +1,8 @@
 package com.coreman2200.ringstrings.app
 
 import android.app.Application
+import com.coreman2200.ringstrings.app.di.ApplicationComponent
+import com.coreman2200.ringstrings.app.di.UtilsModule
 
 /**
  * RSApp
@@ -18,4 +20,16 @@ import android.app.Application
  * You may obtain a copy of the GPLv2 License at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
-class RSApp : Application()
+class RSApp : Application() {
+    private lateinit var appComponent: ApplicationComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        /*
+         'ApplicationComponent' is created including all data every associated component needs.
+         Specifically, 'modules' parameters refer to those which demand external variables (mostly
+         'Context' instances).
+         */
+        appComponent = DaggerApplicationComponent.factory().create(modules = UtilsModule(ctx = this))
+    }
+}
