410a301f31d4411510411cb79054b0146bfdefa3
Cory H <coreman2200@gmail.com>
2022-01-13 09:14:31 -0800

    presentation module

---
+++ presentation/src/main/java/com/coreman2200/ringstrings/presentation/di/Scopes.kt
@@ -0,0 +1,11 @@
+package com.coreman2200.ringstrings.presentation.di
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ApplicationScope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ActivityScope
