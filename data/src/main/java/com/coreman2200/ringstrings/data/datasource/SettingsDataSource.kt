244cb88fe81bae3a565927bef758ce049dfad59c
Cory H <coreman2200@gmail.com>
2022-01-13 15:34:30 -0800

    connects settings data source (wire proto data store) to data repo

--- data/src/main/java/com/coreman2200/ringstrings/data/datasource/SettingsDataSource.kt
+++ data/src/main/java/com/coreman2200/ringstrings/data/datasource/SettingsDataSource.kt
@@ -32,6 +32,7 @@ import com.coreman2200.ringstrings.domain.AppSettingsRequest
 import com.coreman2200.ringstrings.domain.AppSettingsResponse
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.last
+import javax.inject.Inject
 
 interface SettingsDataSource {
 
@@ -40,28 +41,47 @@ interface SettingsDataSource {
     }
 
     suspend fun fetchSettingsData(request: AppSettingsRequest): AppSettingsResponse
+    suspend fun updateSettingsData(request: AppSettingsRequest): AppSettingsResponse
 }
 
-class SettingsWireStore (val context: Context) : SettingsDataSource {
+class SettingsWireStore @Inject constructor(val context: Context) : SettingsDataSource {
     private val Context.settingsWireDataStore: DataStore<AppSettings> by dataStore(
         fileName = "AppSettings.pb",
         serializer = SettingsSerializer
     )
     override suspend fun fetchSettingsData(request: AppSettingsRequest): AppSettingsResponse {
         val flow: Flow<AppSettings> = context.settingsWireDataStore.data
-        val astro = flow.last().astro ?: AstrologySettings()
-        val num = flow.last().num ?: NumerologySettings()
+        val app = flow.last()
 
-        return AppSettingsResponse(
-            true,
-            com.coreman2200.ringstrings.domain.AppSettings(
-                astro = com.coreman2200.ringstrings.domain.AstrologySettings(
-                    astro.max_orb,
-                    astro.ephe_dir
+        return AppSettingsResponse(true, toDomainAppSettings(app))
+
+    }
+
+    override suspend fun updateSettingsData(request: AppSettingsRequest): AppSettingsResponse {
+        val data = context.settingsWireDataStore.updateData { settings ->
+            settings.copy(
+                astro = AstrologySettings(
+                    max_orb = request.maxorb,
+                    ephe_dir = request.ephedir
                 ),
-                num = com.coreman2200.ringstrings.domain.NumerologySettings(num.number_system.ordinal)
+                num = NumerologySettings(
+                    number_system = NumerologySettings.NumberSystemType.values()[request.numsystem]
+                )
             )
-        )
+        }
 
+        return AppSettingsResponse(true, toDomainAppSettings(data))
+    }
+
+    fun toDomainAppSettings(data:AppSettings) : com.coreman2200.ringstrings.domain.AppSettings {
+        val astro = data.astro ?: AstrologySettings()
+        val num = data.num ?: NumerologySettings()
+        return com.coreman2200.ringstrings.domain.AppSettings(
+            astro = com.coreman2200.ringstrings.domain.AstrologySettings(
+                astro.max_orb,
+                astro.ephe_dir
+            ),
+            num = com.coreman2200.ringstrings.domain.NumerologySettings(num.number_system.ordinal)
+        )
     }
 }
